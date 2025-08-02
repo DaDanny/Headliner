1b4ec2c11aac7ea414ea1ddab21acca4d239231c
DaDanny <danny.francken@bonus.ly>
2025-08-02 13:49:55 -0400

    Initial Commit

---
+++ Headliner/HeadlinerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadlinerApp.swift
+//  Headliner
+//
+//  Created by Danny Francken on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HeadlinerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
